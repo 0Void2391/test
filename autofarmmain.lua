45151e01edb7bbc22f81b281277b612a4ea16756
0_Void <106687374+0Void2391@users.noreply.github.com>
2024-11-03 19:09:14 +0100

    Create autofarmmain.lua

---
+++ autofarmmain.lua
@@ -0,0 +1,3 @@
+repeat task.wait() until game:IsLoaded()
+--autofarm code here
+loadstring(game:HttpGet('https://raw.githubusercontent.com/0Void2391/test/refs/heads/main/start.lua'))()--teleport on finish
