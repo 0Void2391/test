8b568883dcff65c7c94eea75be8fee20f2397fb7
0_Void <106687374+0Void2391@users.noreply.github.com>
2024-11-03 21:41:07 +0100

    Delete start.lua

--- start.lua
+++
@@ -1,7 +0,0 @@
-local ts = game:GetService("TeleportService")
-ts:Teleport(11523257493)
-queue_on_teleport([[
-  repeat task.wait() until game:IsLoaded() 
-  queue_on_teleport(game:HttpGet('https://raw.githubusercontent.com/0Void2391/test/refs/heads/main/lobby.lua')) 
-  game.ReplicatedStorage.Join:FireServer()
-]])
